6161fdd0963318bfdbdddbf46bcfbc8178045653
platon09 <nis.platon@gmail.com>
2020-06-24 23:32:37 +0600

    новые атрибуты

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 URL = 'https://kolesa.kz/cars/audi/'
 HEADERS = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
+HOST = 'https://kolesa.kz'
 
 
 def get_html(url, params=None):
@@ -17,11 +18,14 @@ def get_content(html):
     cars = []
     for item in items:
         cars.append({
-            'title': item.find('span', class_='a-el-info-title').get_text()
+            'title': item.find('span', class_='a-el-info-title').get_text(strip=True),
+            'description': item.find('div', class_='a-search-description').get_text(strip=True),
+            'link': HOST + item.find('a', class_='list-link ddl_product_link').get('href')
         })
     print(cars)
+    print(len(cars))
 
-# class_='list-link ddl_product_link'
+
 def parse() -> None:
     html = get_html(URL)
     if html.status_code == 200:
